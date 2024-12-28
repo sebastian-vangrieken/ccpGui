be6c2aeaa1708612365e10792c11482e759678a2
sebastian-vangrieken <sebastian.vangrieken12@gmail.com>
2024-12-28 21:36:40 +0100

    started fresh xD

--- main.cpp
+++ main.cpp
@@ -4,41 +4,36 @@
 
 #include <windows.h>
 
-LRESULT CALLBACK WindowProc(
-    HWND hwnd,
-    UINT uMsg,
-    WPARAM wParam,
-    LPARAM lParam
-);
-
 int WINAPI wWinMain(
     HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
     PWSTR pCmdLine,
     int nCmdShow
 ) {
-    const wchar_t CLASS_NAME[] = L"Sample Window class";
+    // MessageBox(NULL, pCmdLine, L"title", MB_OK);
+    const wchar_t CLASS_NAME[] = L"Sample Window Class";
 
     WNDCLASS wc = {};
 
-    wc.lpfnWndProc = WindowProc;
+    // wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
 
     RegisterClass(&wc);
 
     HWND hwnd = CreateWindowEx(
-        0, // DWORD dwExStyle
-        CLASS_NAME, // LPCSTR lpClassName
-        L"Learn to Program Windows", // LPCSTR lpWindowName
-        WS_OVERLAPPEDWINDOW, // DWORD dwStyle
+        0, // Optional window style.
+        CLASS_NAME,
+        L"Learn to Program Windows",
+        WS_OVERLAPPEDWINDOW, // Window style.
 
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, // int X, int Y, int nWidth, int nHeight
+        // x-position, y-position, width, height.
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
 
-        NULL, // HWND hwndParent
-        NULL, // HMENU hMenu
-        hInstance, // HINSTANCE hInstance
-        NULL // LPVOID lpParam
+        NULL, // Parent window.
+        NULL, // Menu.
+        hInstance,
+        NULL // Additional Application data.
     );
 
     if (hwnd == NULL) {
@@ -47,31 +42,5 @@ int WINAPI wWinMain(
 
     ShowWindow(hwnd, nCmdShow);
 
-    MSG msg = {};
-    while (GetMessage(&msg, NULL, 0, 0) > 0) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
     return 0;
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
-        EndPaint(hwnd, &ps);
-    }
-    return 0;
-
-    }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
